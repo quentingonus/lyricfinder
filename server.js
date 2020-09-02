c20486b7332b9c0047151213361c627012e8ec00
quentingonus <55360738+quentingonus@users.noreply.github.com>
2020-09-02 18:41:02 +0630

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const lyricsFinder = require('lyrics-finder');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/file', function(req, res) {
+    if (Object.keys(req.query).length === 0) {
+        res.render('pages/file', {
+            files: []
+        });
+    }
+});
+app.get('/', function(req, res) {
+    if (Object.keys(req.query).length === 0) {
+        res.render('pages/index', {
+            lyrics: "",
+            song_to_find: "",
+            artist_to_find: "",
+        });
+    } else {
+        var artist_to_find = req.query.artist_to_find;
+        var song_to_find = req.query.song_to_find;
+        (async(artist, title) => {
+            let lyrics = await lyricsFinder(artist, title) || "Not Found!";
+            res.render('pages/index', {
+                lyrics: lyrics,
+                song_to_find: song_to_find,
+                artist_to_find: artist_to_find,
+            });
+        })(artist_to_find, song_to_find);
+    }
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is running at http://localhost:8080');
